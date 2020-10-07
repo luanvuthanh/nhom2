621fd264f99e08764b1cf9ef32da9d27e35a2b49
Luan <luanvu3198@gmail.com>
2020-10-07 16:15:50 +0700

    sua

--- source.cpp
+++ source.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
-int tong(int a, int b);
-int hieu(int a, int b);
-int tich(int a, int b);
-float thuong(int a, int b);
+int tong(int a, int b)
+{
+	return a+b;
+}
+
+int hieu(int a, int b)
+{
+	return a-b;
+}
+int tich(int a, int b)
+{
+	return a*b;
+}
+float thuong(int a, int b)
+{
+	return a/b;
+}
 int main()
 {
 	int a, b;
@@ -15,9 +28,7 @@ int main()
 	switch (phepToan)
 	{
 	case '+':
-		int tong;
-		tong=a+b;
-		return tong;
+		return tong(a,b);
 		break;
 	case '-':
 
