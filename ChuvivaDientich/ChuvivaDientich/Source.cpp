03fa38615bc33ee4f747b9b4ac9fb19af68b11fb
luanvuthanh <72492157+luanvuthanh@users.noreply.github.com>
2020-10-14 15:47:45 +0700

    ChuvivaDien tich sua

---
+++ ChuvivaDientich/ChuvivaDientich/Source.cpp
@@ -0,0 +1,35 @@
+#include"iostream"
+#include"Math.h"
+using namespace std;
+
+int main()
+{
+	int ch;
+	cout << "Tinh gi? (P vuong,S vuong,P chu nhat,S chu nhat,P tron,S tron) ";
+	cin >> ch;
+	switch (ch)
+	{
+	case 1:
+
+		break;
+	case 2:
+
+		break;
+	case 3:
+
+		break;
+	case 4:
+
+		break;
+	case 5:
+
+		break;
+	case 6:
+
+		break;
+	default:
+
+		break;
+	}
+	return 0;
+}
